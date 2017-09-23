fae87c3a9da074230f16ef21f2068611e3b69243
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-09-23 12:03:18 -0700

    implemented stack using linked list

--- stack/linked-list-stack.rb
+++ stack/linked-list-stack.rb
@@ -1,42 +1,162 @@
+class Node
+  attr_reader :value # allow external entities to read value but not write
+  attr_accessor :next # allow external entities to read or write next node
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
 class Stack
+  attr_reader :head, :max_size # allow external entities to read value but not write
 
-  def initialize()
-    puts "not implememted"
+  def initialize(max_size)
+    @head = nil
+    @max_size = max_size
   end
 
+  # --------
   # returns true is the data structure is empty, false otherwise
+  # time: O(1) / Constant - time to access the head is independent of the size of the stack
+  # space: O(1) / Constant - variables used are independent of the size of the stack
   def is_empty()
-    puts "not implemented"
+    @head == nil
   end
 
+  # --------
+  # returns true is the data structure is full, false otherwise
+  # time: O(n) / Linear - is dependent on the size of the stack
+  # space: O(1) / Constant - variables used are independet of the size of the stack
+  def is_full()
+    self.size == @max_size
+  end
+
+  # --------
   # returns the number of items in the data structure
+  # time: O(n) / Linear - the time it takes to count all nodes is dependent on the size of stack
+  # space: O(1) / Constant - the variables used are independet of the size of the stack
   def size()
-    puts "not implemented"
+    return nil if @head == nil
+
+    current = @head
+    size = 0
+
+    while current != nil
+      size += 1
+      current = current.next
+    end
+
+    return size
   end
 
+  # --------
   # returns the item that would be popped next
+  # time: O(1) / Constant - time to access the head does not depend on the size of the stack
+  # space: O(1) / Constant - variables used do not depend on size of the stack
   def top()
-    puts "not implemented"
+    return "Stack is empty." if is_empty
+
+    return @head.value
   end
 
+  # --------
   # returns the minimum integer data value in the data structure
+  # time: O(n) / Linear - time to search all nodes for the minimum value is dependent on size of the stack (n)
+  # space: O(1) / Constant - variables used do not depend on size of the stack
   def min()
-    puts "not implemented"
+    return "Stack is empty." if is_empty
+
+    current = @head
+    min = current.value
+
+    while current != nil
+      if current.value < min
+        min = current.value
+      end
+
+      current = current.next
+    end
+
+    return min
   end
 
+  # --------
   # returns the maximum integer data value in the data structure
+  # time: O(n) / Linear - time to search all nodes for the max value is dependent on size of the stack (n)
+  # space: O(1) / Constant - variables used do not depend on size of the stack
   def max()
-    puts "not implemented"
+    return "Stack is empty." if is_empty
+
+    current = @head
+    max = current.value
+
+    while current != nil
+      if current.value > max
+        max = current.value
+      end
+
+      current = current.next
+    end
+
+    return max
   end
 
+  # ----------
   # adds item to stack
-  def push()
-    puts "not implemented"
+  # time: O(1) / Constant - the node is always added at the beginning so the time it takes is independent of the size of the stack
+  # space: O(1) / Constant - the variables are independent of the size of the stack
+  def push(value)
+    return "Stack is full" if is_full
+
+    new_node = Node.new(value)
+
+    if @head == nil
+      @head = new_node
+    else
+      new_node.next = @head
+      @head = new_node
+    end
+
+    return
   end
 
+  # --------
   # removes and returns the top item
+  # time: O(1) / Constant - always removing from the beginning so it does not depend on the size of the stack
+  # space: O(1) / Constant - the variables are independent of the size of the stack
   def pop()
-    puts "not implemented"
+    return "Stack is empty" if is_empty
+
+    if @head.next == nil
+      temp = @head.value
+      @head = nil
+      return temp
+    end
+
+    temp = @head.value
+    @head = @head.next
+    return temp
+  end
+
+  # --------
+  # use this to print out visual of the stack
+  # time: O(n) / Linear - time to search print all nodes is dependent on size of the stack (n)
+  # space: O(1) / Constant - the variables are independent of the size of the stack
+  def view()
+    puts "top"
+    puts "====="
+
+    current = @head
+
+    while current != nil
+      puts current.value
+
+      current = current.next
+    end
+
+    puts "====="
+    puts "bottom"
   end
 
 end

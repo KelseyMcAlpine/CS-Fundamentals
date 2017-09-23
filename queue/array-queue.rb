ccf0abf67db481ef08205fd3ce036d6c526bbdb7
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-09-23 13:20:18 -0700

    Implemented queue using array.

--- queue/array-queue.rb
+++ queue/array-queue.rb
@@ -1,42 +1,116 @@
-class Stack
+class Queue
+  attr_reader :queue, :max_size, :front
 
-  def initialize()
-    puts "not implememted"
+  def initialize(max_size)
+    @max_size = max_size
+    @queue = Array.new
+    @front = nil
   end
 
   # returns true is the data structure is empty, false otherwise
+  # time: O(1) / Constant - does not depend on the size of the queue
+  # space : O(1) / Constant - varaiables are independent of the size of the queue
   def is_empty()
-    puts "not implemented"
+    @front == nil
+  end
+
+  # returns true is the data structure is empty, false otherwise
+  # time: O(n) / Linear - is dependent on the size of the queue
+  # space : O(1) / Constant - vaaibles are independent of the size of the queue
+  def is_full()
+    @queue.size == @max_size
   end
 
   # returns the number of items in the data structure
+  # time: O(n) / Linear -
+  # space : O(1) / Constant -
   def size()
-    puts "not implemented"
+    return nil if @front == nil
+
+    i = 0
+
+    while @queue[i] != nil
+      i += 1
+    end
+
+    return i
   end
 
   # returns the item that would be dequeued next (for Queue)
+  # time: O(1) / Constant -
+  # space : O(1) / Constant -
   def front()
-    puts "not implemented"
+    return "Queue is empty." if is_empty
+
+    @queue[0]
   end
 
   # returns the minimum integer data value in the data structure
+  # time: O(n) / Linear -
+  # space : O(1) / Constant -
   def min()
-    puts "not implemented"
+    return "Queue is empty." if is_empty
+
+    min = @queue[0]
+
+    @queue.each do | i |
+      min = i if i < min
+    end
+
+    return min
   end
 
   # returns the maximum integer data value in the data structure
+  # time: O(n) / Linear -
+  # space : O(1) / Constant -
   def max()
-    puts "not implemented"
+    return "Queue is empty." if is_empty
+
+    max = @queue[0]
+
+    @queue.each do | i |
+      max = i if i > max
+    end
+
+    return max
   end
 
   # adds item to the queue
-  def enqueue()
-    puts "not implemented"
+  # time: O(1) / Constant -
+  # space : O(1) / Constant -
+  def enqueue(value)
+    return "Queue is full" if is_full
+
+    @queue.push(value)
+
+    @front = 0 if size == nil
   end
 
   # removes and returns the next item in line
+  # time: O(1) / Constant -
+  # space : O(1) / Constant -
   def dequeue()
-    puts "not implemented"
+    return "Queue is empty" if is_empty
+
+    temp = @queue[0]
+    @queue.delete_at(0)
+    @front = nil if size == 0
+
+    return temp
   end
 
+  # prints all values in the queue
+  # time: O(n) / Linear -
+  # space : O(1) / Constant -
+  def view
+    puts "front"
+    puts "====="
+
+    @queue.each do | i |
+      puts i
+    end
+
+    puts "====="
+    puts "back"
+  end
 end
---
+++ queue/queue-test.rb
@@ -0,0 +1,60 @@
+# uncomment file to test
+require './array-queue.rb'
+# require './2-stack-queue.rb'
+# require './linked-list-queue.rb'
+
+# started with empty queue
+my_queue = Queue.new(10)
+puts "BUG: my_queue.size should be nil" if my_queue.size.nil? != true
+puts "BUG: my_queue.is_empty should be true" if my_queue.is_empty != true
+puts "BUG: my_queue.is_full should be false" if my_queue.is_full != false
+
+# fill queue and check that front remains the first element
+my_queue.enqueue(4)
+my_queue.enqueue(3)
+my_queue.enqueue(10)
+my_queue.enqueue(8)
+my_queue.enqueue(7)
+my_queue.enqueue(2)
+my_queue.enqueue(1)
+my_queue.enqueue(6)
+my_queue.enqueue(5)
+my_queue.enqueue(9)
+
+# check characteristics of full queue
+puts "BUG: my_queue.front should be 4" if my_queue.front != 4
+puts "BUG: my_queue.size should be 10" if my_queue.size != 10
+puts "BUG: my_queue.is_empty should be false" if my_queue.is_empty != false
+puts "BUG: my_queue.is_full should be true " if my_queue.is_full != true
+puts "BUG: my_queue.min should be 1" if my_queue.min != 1
+puts "BUG: my_queue.max should be 10" if my_queue.max != 10
+
+# looking at top elemenent does not remove it
+my_queue.front
+puts "BUG: looking at top element should not remove it" if my_queue.size != 10
+
+# is there a better way to include this test?
+puts my_queue.enqueue(100)
+
+# begin removing all elements from queue
+puts "BUG: my_queue.dequeue() should return 4" if my_queue.dequeue != 4
+puts "BUG: my_queue.dequeue() should return 3" if my_queue.dequeue != 3
+puts "BUG: my_queue.dequeue() should return 9" if my_queue.dequeue != 10
+puts "BUG: my_queue.dequeue() should return 8" if my_queue.dequeue != 8
+puts "BUG: my_queue.dequeue() should return 7" if my_queue.dequeue != 7
+puts "BUG: my_queue.dequeue() should return 2" if my_queue.dequeue != 2
+puts "BUG: my_queue.dequeue() should return 1" if my_queue.dequeue != 1
+
+# should have new characteristics
+puts "BUG: my_queue.size should return 6" if my_queue.size != 3
+puts "BUG: max should be 9" if my_queue.max != 9
+puts "BUG: min should be 2" if my_queue.min != 5
+
+# finish removing all elements from queue
+puts "BUG: my_queue.dequeue() should return 6" if my_queue.dequeue != 6
+puts "BUG: my_queue.dequeue() should return 5" if my_queue.dequeue != 5
+puts "BUG: my_queue.dequeue() should return 10" if my_queue.dequeue != 9
+puts "BUG: my_queue.is_empty should be true" if my_queue.is_empty != true
+
+# is there a better way to include this test?
+puts my_queue.dequeue

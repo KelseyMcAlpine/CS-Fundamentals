c2fa776c281ece38f3027f2ee04bcf765c632ec6
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-09-23 15:25:25 -0700

    Directory for recursive exercises

---
+++ recursion/recursion-exercises.rb
@@ -0,0 +1,5 @@
+# 1. reverse the elements in an integer array in place
+
+# 2. check if a given input string is a palindrome. return true or false
+
+# 3. return the nth Fib number in the Fibonacci series starting at 0 (0, 1, 1, 2, 3, 5, 8, 13..)

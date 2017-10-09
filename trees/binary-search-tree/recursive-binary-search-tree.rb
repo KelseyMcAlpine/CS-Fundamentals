f53f0d89ddb71af5f2cde70d2f872b012bd40c56
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-10-08 17:41:24 -0700

    Updated file structure

---
+++ trees/binary-search-tree/recursive-binary-search-tree.rb
@@ -0,0 +1,81 @@
+class Node
+  attr_reader :value
+  attr_accessor :left, :right
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
+
+class BinarySearchTree
+  def initialize()
+    @root = nil
+  end
+
+  # Return true if the bst is empty, false otherwise.
+  # Time: O() / -
+  # Space: O() / -
+  def is_empty()
+    @root == nil
+  end
+
+  # Search for a given integer value in the Binary Search Tree.
+  # Return true if the value is found, false otherwise.
+  # Time: O() / -
+  # Space: O() / -
+  def search(value)
+    puts "not implemented"
+  end
+
+  # Insert a given integer value in the Binary Search Tree.
+  # Time: O() / -
+  # Space: O() / -
+  def insert(value)
+    puts "not implemented"
+  end
+
+  # Delete a given integer value in the Binary Search Tree.
+  # Time: O() / -
+  # Space: O() / -
+  def delete(value)
+    puts "not implemented"
+  end
+
+  # Compute the height of the Binary Search Tree.
+  # Time: O() / -
+  # Space: O() / -
+  def height()
+    puts "not implemented"
+  end
+
+  # Print values in the Binary Search Tree in pre-order.
+  # Time: O() / -
+  # Space: O() / -
+  def print_preorder()
+    puts "not implemented"
+  end
+
+  # Print values in the Binary Search Tree in in-order.
+  # Time: O() / -
+  # Space: O() / -
+  def print_in_order()
+    puts "not implemented"
+  end
+
+  # Print values in the Binary Search Tree in post-order.
+  # Time: O() / -
+  # Space: O() / -
+  def print_post_order()
+    puts "not implemented"
+  end
+
+  # Print values in the Binary Search Tree in level-order.
+  # (Breadth-first traversal)
+  # Time: O() / -
+  # Space: O() / -
+  def print_level_order()
+    puts "not implemented"
+  end
+end
